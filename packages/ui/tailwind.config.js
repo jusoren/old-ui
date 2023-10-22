856004130b626ae07013fcddd876f03b523c2448
jusoren <jusoren@outlook.co.id>
2023-10-22 13:12:23 +0700

    update ui package

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  // `content` will be replaced if present in the project configuration.
+  // Check https://tailwindcss.com/docs/presets#merging-logic-in-depth
   content: ["./components/**/*.{jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        base: "0.97rem",
+      },
+    },
   },
   plugins: [],
 };
